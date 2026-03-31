850e605270442794e16078c0701baf11b72f3aa1
HussienTalha <hussien.talha99@gmail.com>
2026-03-31 13:43:57 +0200

    fixed delete function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -82,7 +82,7 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category):RedirectResponse
+    public function delete(Category $category):RedirectResponse
     {
         /**
          * @var Category $uncategorized
